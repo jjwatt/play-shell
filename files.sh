4a6b981577fca8139f5c9ea65d572860e4d9387e
Jesse Wattenbarger <jwatt@broken.watch>
2025-04-16 04:52:49 -0400

    Messing with files

---
+++ files.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+: "${CURRENT_DIR:=/opt/hap/updater/current}"
+
+get_current_version_string() {
+    shopt -s nullglob dotglob
+    files=("$CURRENT_DIR"/*)
+    shopt -u nullglob dotglob
+    if [[ "${#files[@]}" -gt 0 ]]; then
+	local highest_file="$(printf "%s\n" | sort -r | sed 1q)"
+	local version_string="${highest_file##*/}"
+	echo "$version_string"
+	return 0
+    else
+	return 1
+    fi
+}
+
+get_current_version() {
+    local vs="$(get_current_version_string)"
+    if [[ -z $vs ]]; then
+	echo "${vs##*_}"
+	return 0
+    else
+	return 1
+}
