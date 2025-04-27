7fb742f9f224e13d2cf94a1a5bd8c65837e55e1e
Jesse Wattenbarger <jwattenbarger@spencerhealthsolutions.com>
2025-04-27 10:18:59 -0400

    Add array_to_string

---
+++ array_to_string.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# @file array_to_string.bash
+# @brief from bash examples
+
+# @fn array_to_string
+# @brief Turn an array into a string with seperator
+# @param vname_of_array The variable name of the array
+# @param vname_of_string The variable name of the string
+# @param separator The optional separator to use. Defaults to IFS
+array_to_string() {
+    (( ($# < 2) || ($# > 3) )) && {
+	"$FUNCNAME: usage: $FUNCNAME arrayname stringname [seperator]"
+	return 2
+    }
+    local array=$1 string=$2
+    (($#==3)) && [[ $3 = ? ]] && local IFS="${3}${IFS}"
+    
+    eval $string="\"\${$array[*]}\""
+    return 0
+}
