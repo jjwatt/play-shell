2ad8bc893e0621b096c19954f09a48a4061a8c9f
Jesse Wattenbarger <jwatt@broken.watch>
2025-10-05 11:08:36 -0400

    Add is_anagram_with_hash to neetcode/p2.bash

--- neetcode/p2.bash
+++ neetcode/p2.bash
@@ -8,7 +8,7 @@ sort_word() {
     echo -n "$1" | grep -o . | sort | tr -d '\n'
 }
 
-is_anagram() {
+is_anagram_with_sort() {
     local word1="$1"
     local word2="$2"
     local sort1
@@ -24,8 +24,42 @@ is_anagram() {
     fi
 }
 
+is_anagram_with_hash() {
+    local word1="$1"
+    local word2="$2"
+
+    declare -A count1
+    declare -A count2
+    local i
+
+    for (( i = 0; i < "${#word1}"; i++ )) do
+	local letter="${word1:$i:1}"
+	count1[$letter]=$(( "${count1[$letter]}" + 1 ))
+    done
+    for (( i = 0; i < "${#word2}"; i++ )) do
+	local letter="${word2:$i:1}"
+    	count2[$letter]=$(( "${count2[$letter]}" + 1 ))
+    done
+    local match=0
+    for key in "${!count1[@]}"; do
+	# echo "$key" "${count1[$key]}"
+	# echo "${count2[$key]}"
+	if ! [[ -v "${count2[$key]}" ]]; then
+	    # echo "Mismatch"
+	    match=1
+	    break
+	fi
+	if (( "${count1[$key]}" != "${count2[$key]}" )); then
+	    #echo "Mismatch"
+	    match=1
+	fi
+    done
+    return $match
+}
+
 main() {
-    is_anagram "$1" "$2"
+    time is_anagram_with_hash "$1" "$2"
+    time is_anagram_with_sort "$1" "$2"
 }
 
 main "$@"
