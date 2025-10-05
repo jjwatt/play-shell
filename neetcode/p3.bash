bb9fd111a8dc51991ed5b4c7c72c89563fdb1307
Jesse Wattenbarger <jwatt@broken.watch>
2025-10-05 18:30:22 -0400

    Working two_sum in bash

--- neetcode/p3.bash
+++ neetcode/p3.bash
@@ -4,23 +4,18 @@ two_sum() {
     declare -n list="$1"
     declare -i target="$2"
 
-    declare -A indices
-    local i
-    for (( i = 0; i < "${#list[@]}"; i++ )); do
-	local val="${list[$i]}"
-	indices["$val"]="$i"
-    done
-    for (( i = 0; i < "${#list[@]}"; i++ )); do
-	local val="${list[$i]}"
+    declare -A seen
+    for i in "${!list[@]}"; do
+	local val="${list[i]}"
 	local diff=$(( target - val ))
-	for value in "${indices[@]}"; do
-	    if [[ "$diff" == "$value" ]]; then
-		if ! [[ "${indices[$diff]}" == "$i" ]]; then
-		    echo "$i, ${indices[$diff]}"
-		fi
-	    fi
-	done
+
+	if [[ -v "seen[$diff]" ]]; then
+	    echo "${seen[$diff]}", "$i"
+	    return 0
+	fi
+	seen["$val"]="$i"
     done
+    return 1
 }
 
 main() {
